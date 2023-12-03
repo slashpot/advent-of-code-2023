67c12ce1e7b17957d959b9efad8fdd59fae9b0bd
Eric Lin <tttttt123465@gmail.com>
2023-12-03 09:41:12 +0800

    finish day1

--- ts/1/day1.ts
+++ ts/1/day1.ts
@@ -1,15 +1,14 @@
-const al = [
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-    'ten'
-]
+const al = {
+    'one': '1',
+    'two': '2',
+    'three':'3',
+    'four':'4',
+    'five':'5',
+    'six':'6',
+    'seven':'7',
+    'eight':'8',
+    'nine':'9'
+}
 
 const map = new Map<string, string[]>([
         ['o', ['one']],
@@ -21,90 +20,42 @@ const map = new Map<string, string[]>([
     ]
 )
 
-const mapReverse = new Map<string, string[]>([
-    ['e', ['eno, eerht','evif','enin']],
-    ['o', ['owt',]],
-    ['r', ['ruof',]],
-    ['x', ['xis']],
-    ['n', ['neves', 'net']],
-    ['t', ['thgie']],
-])
-
-const map2 = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine':9,
-    'ten': 10
-}
-
-
 export function trebuchet(inputs: string[]) {
     let sum = 0;
     for (const input of inputs) {
-        const arr = input.split("");
-        let cur: string = "";
-        let begin = 0,
-            end = arr.length - 1;
-        while (begin < input.length) {
-            const val = input[begin]
-            if (map.has(val)) {
-                const words = map.get(val)
-                begin++;
-                let wordIndex = 1;
+        let index = 0;
+        const nums=[];
+
+        while (index<input.length) {
+            const val=input[index];
+            if(isNumber(val)) {
+                nums.push(val)
+            } else if(map.has(val)) {
+                let isFoundWord = false;
+                const words = map.get(val);
                 for (const word of words) {
-                    while (wordIndex < word.length) {
-                        if(word[wordIndex] !== input[begin]) break;
-                        else {
-                            if(wordIndex === word.length-1) {
-                                cur = map2[word] + cur;
+                    let wordIndex = 1;
+                    let index2 = index+1;
+                    while (wordIndex < word.length){
+                        if (input[index2] === word[wordIndex]) {
+                            if(wordIndex === word.length-1){
+                                nums.push(al[word]);
+                                isFoundWord=true;
                                 break;
-                            } else {
+                            }else {
+                                index2++;
                                 wordIndex++;
-                                begin++;
                             }
+                        } else {
+                            break;
                         }
                     }
-                }
-            } else if (isNumber(val)) {
-                cur = val + cur;
-                break;
-            }
-            begin++;
-        }
-        while (end >= 0) {
-            const val = input[end]
-            if (mapReverse.has(val)) {
-                const words = mapReverse.get(val)
-                end--;
-                let wordIndex = 1;
-                for (const word of words) {
-                    while (word.length - 1 - wordIndex >= 0) {
-                        if(word[word.length - 1 -wordIndex] !== input[end]) break;
-                        else {
-                            if(wordIndex === word.length-1) {
-                                cur = cur + map2[word];
-                                break;
-                            } else {
-                                wordIndex++;
-                                end--;
-                            }
-                        }
-                    }
+                    // if(isFoundWord)break;
                 }
             }
-            else if (isNumber(val)) {
-                cur = cur + val;
-                break;
-            }
-            end--;
+            index++;
         }
-        sum += Number(cur);
+        sum+=Number(nums[0]+nums[nums.length-1])
     }
     return sum;
 }
